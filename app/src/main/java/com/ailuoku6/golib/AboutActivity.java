a8427b25aa3e02812b5d05f215efee322969e2a1
ailuoku6-blog <18897514992@163.com>
2019-01-25 00:16:16 +0800

    add

--- app/src/main/java/com/ailuoku6/golib/AboutActivity.java
+++ app/src/main/java/com/ailuoku6/golib/AboutActivity.java
@@ -1,20 +1,25 @@
 package com.ailuoku6.golib;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class AboutActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        toolbar = (Toolbar) findViewById(R.id.User_detail_toolbar);
+        toolbar = (Toolbar) findViewById(R.id.About_toolbar);
+        imageButton = (ImageButton) findViewById(R.id.giuhub_img);
 
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
         setSupportActionBar(toolbar);
@@ -22,6 +27,15 @@ public class AboutActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://github.com/ailuoku6/Go_lib"));
+                startActivity(intent);
             }
         });
     }
